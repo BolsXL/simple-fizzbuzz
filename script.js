5d7f9bf41dab5e7614f9c043a9b3877afebaab97
bols <joelmer13@gmail.com>
2021-05-29 20:28:37 -0400

    Removed isFizzbuzz() function, turned multiple console.log() statements into a single one.

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 // RUNNING CODE
-fizzbuzz(222);
+fizzbuzz(15);
 
 // FUNCTIONS
 function fizzbuzz(n = 1) {
+    let text = ``;
     for (let i = 1; i <= n; i++) {
-        if (isFizzbuzz(i)) console.log(`FizzBuzz`);
-        else if (isFizz(i)) console.log(`Fizz`);
-        else if (isBuzz(i)) console.log(`Buzz`);
-        else console.log(i);
+        if (isFizz(i) && isBuzz(i)) text += `FizzBuzz, `;
+        else if (isFizz(i)) text += `Fizz, `;
+        else if (isBuzz(i)) text += `Buzz, `;
+        else text += `${i}, `;
     }
-}
-
-function isFizzbuzz(n) {
-    if (n % 5 == 0 && n % 3 == 0) return true;
-    else return false;
+    console.log(`${text.slice(0, text.length - 2)}.`);
 }
 
 function isFizz(n) {
@@ -24,4 +21,10 @@ function isFizz(n) {
 function isBuzz(n) {
     if (n % 5 == 0) return true;
     else return false;
+}
+
+function askForNumber() {
+    let n = prompt("Enter a number");
+    n = Number(n);
+    return n;
 }
